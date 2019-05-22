5246e4784b314c81d2f31c3d92113d4c671425e4
caijieying <caijieying@jd.com>
2019-05-22 16:30:58 +0800

    add: demo展示

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,49 +1,12 @@
-const {version} = require('../package.json'),
+const merge = require('webpack-merge'),
+  base = require('./webpack.config.base'),
   path = require('path'),
-  webpack = require('webpack'),
-  CleanWebpackPlugin = require('clean-webpack-plugin'),
   CopyWenpackPlugin = require('copy-webpack-plugin'),
   HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = merge(base, {
   mode: 'development',
-  entry: {
-    index: path.resolve(__dirname, '../src/js/index.js')
-  },
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'js/[name].js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules|lib/,
-      loader: 'babel-loader'
-    },{
-      test: /\.s?css$/,
-      use: [ // loader从后往前执行
-        'style-loader', // 把样式插入到页面的style标签中
-        'css-loader', // 遍历所有css文件，找到所有@import及url(.)并处理
-        'postcss-loader', // 处理css文件，加前缀等
-        'sass-loader' // 遍历scss/sass文件，处理为css文件
-      ]
-    }, {
-      test: /\.(jpe?g|png|gif|svg)$/,
-      loader: 'file-loader', // 处理文件名/文件路径，移动到打包后的目录下
-      options: {
-        name: '[name].[ext]',
-        outputPath: 'img/'
-      }
-    }]
-  },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['../dist', '../dist.zip']
-    }),
-    new webpack.BannerPlugin(`v-${version}, by Cai Jieying`),
-    new webpack.ProvidePlugin({
-      $: path.resolve(__dirname, '../src/lib/zepto.js') 
-    }),
     new CopyWenpackPlugin([{
       from: '../src/lib/phaser.min.js',
       to: 'js'
@@ -64,4 +27,4 @@ module.exports = {
         ignored: /node_modules/
     }
   }
-}
+})
---
+++ build/webpack.config.prod.js
@@ -0,0 +1,58 @@
+const path = require('path'),
+  merge = require('webpack-merge'),
+  base = require('./webpack.config.base'),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  CopyWebpackPlugin = require('copy-webpack-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  FilemanagerWebpackPlugin = require('filemanager-webpack-plugin');
+
+module.exports = merge(base, {
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.s?css$/,
+      use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '../'
+          }
+        },
+        {
+          loader: 'css-loader'
+        },
+        'postcss-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    }),
+    new CopyWebpackPlugin([{
+      from: '../src/js/phaser.min.js',
+      to: 'js',
+    }, {
+      from: '../src/img',
+      to: 'img',
+      ignore: ['.gitkeep']
+    }]),
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true
+      },
+      template: path.resolve(__dirname, '../src/index.html')
+    }),
+    new FilemanagerWebpackPlugin({
+      onEnd: [{
+        archive: [{
+          source: path.resolve(__dirname, '../dist'),
+          destination: path.resolve(__dirname, '../dist.zip')
+        }]
+      }]
+    })
+  ]
+});
